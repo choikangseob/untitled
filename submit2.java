e30c17f752d69c461558800804ade127893e3543
ChoiKangSub <cks4718@gmail.com>
2024-10-15 16:20:46 +0900

    16시20분

---
+++ submit2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class submit2 {
+  public static void main(String[] args) {
+    Scanner Scanner = new Scanner(System.in);
+
+    double A = Scanner.nextInt();
+    double B = Scanner.nextInt();
+    double result =0;
+    if(0<A && 0<B && A<10 && B<10) {
+      result = A/B;
+    }
+    System.out.println(result);
+  }
+}
