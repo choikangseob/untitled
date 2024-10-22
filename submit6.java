21c7d34542cf4723c55c281f96ea6d04b1dbfcfc
ChoiKangSub <cks4718@gmail.com>
2024-10-22 16:34:58 +0900

    16시34분

---
+++ submit6.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class submit6 {
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> number = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    int sum=0;
+    number.add(0);
+    number.add(1);
+    if(number.size()>=2){
+      for (int i = 2; i <=30; i++) {
+        sum += number.get(i-2)+number.get(i-1);
+        number.add(sum);
+        sum = 0;
+      }
+    }
+    int n =sc.nextInt();
+    if(n>=2) {
+      int m = number.get(n);
+      System.out.println(m);
+    }
+
+  }
+}
+
