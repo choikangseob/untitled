349fa70fb98ed65e4e760650ef61705494437762
ChoiKangSub <cks4718@gmail.com>
2025-01-09 20:43:23 +0900

    -회의실 배정(그리디)

---
+++ Algorithm/december/P20250109.java
@@ -0,0 +1,38 @@
+package Algorithm.december;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class P20250109 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int[][] arr = new int[n][2];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < 2; j++) {
+        arr[i][j] = sc.nextInt();
+      }
+    }
+
+    Arrays.sort(arr, new Comparator<int[]>() {
+      @Override
+      public int compare(int[] a, int[] b) {
+        return Integer.compare(a[1], b[1]);
+      }
+    });
+
+    int count =1;
+    int compare = arr[0][1];
+  for (int i = 1; i < n; i++) {
+      if(arr[i][0]>= compare){
+         count++;
+         compare = arr[i][1];
+      }
+  }
+
+  System.out.println(count);
+  }
+}
