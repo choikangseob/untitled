f424a01053dba605b00f60dc12c81f3132d8292e
ChoiKangSub <cks4718@gmail.com>
2025-02-10 20:15:37 +0900

    -쇠막대기 다못품

---
+++ Algorithm/december/P20250210.java
@@ -0,0 +1,40 @@
+package Algorithm.december;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P20250210 {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    ArrayList<String> list = new ArrayList<>();
+
+    while(true) {
+      String input = sc.nextLine();
+
+      if(input.equals("x")) {
+        break;
+      }else{
+        list.add(input);
+      }
+    }
+    int stack = 0;
+    int sum = 0;
+
+    for (int i = 0; i < list.size(); i++) {
+
+
+      if (list.get(i).equals("(")) {
+        stack++;
+      } else if (list.get(i).equals(")")) {
+        stack--;
+
+        if (list.get(i - 1).equals("(")) {
+          sum += stack;
+        }
+      }
+    }
+
+    System.out.println(sum);
+  }
+}
