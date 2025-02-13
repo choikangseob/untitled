1169bca1421a3af65aad815a643326de24e1f1bc
ChoiKangSub <cks4718@gmail.com>
2025-02-13 19:37:59 +0900

    -공주 구하기

---
+++ Algorithm/february/P20250213.java
@@ -0,0 +1,24 @@
+package Algorithm.february;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P20250213 {
+
+  public static void main(String[] args) {
+
+    Queue<Integer> q = new LinkedList<>();
+
+    for (int i = 1; i < 9; i++) {
+      q.add(i);
+    }
+    while (q.size() > 1) {
+      for(int i =0;i<2;i++){
+        q.add(q.poll());
+      }
+      q.poll();
+    }
+    System.out.println(q.peek());
+    }
+}
