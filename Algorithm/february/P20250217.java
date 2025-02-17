486417223860811756a9101df5ab319fa2e14d31
ChoiKangSub <cks4718@gmail.com>
2025-02-17 13:58:39 +0900

    -단어 찾기

---
+++ Algorithm/february/P20250217.java
@@ -0,0 +1,27 @@
+package Algorithm.february;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class P20250217 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    HashMap<String, Integer> map = new HashMap<>();
+
+    int n = sc.nextInt();
+
+    for (int i = 0; i < n; i++) {
+      map.put(sc.next(), sc.nextInt());
+    }
+
+    for (int i = 0; i < n-1; i++) {
+      String key = sc.next();
+      if(map.containsKey(key)){
+        map.remove(key);
+      }
+    }
+    System.out.println(map);
+  }
+}
