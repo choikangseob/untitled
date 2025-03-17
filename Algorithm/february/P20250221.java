6e956857181aac4890d042623c7794632038e5a9
ChoiKangSub <cks4718@gmail.com>
2025-03-17 14:59:30 +0900

    -재기함수를 이용한 이진수 출력

--- Algorithm/february/P20250221.java
+++ Algorithm/february/P20250221.java
@@ -4,6 +4,7 @@ public class P20250221 {
   public static void main(String[] args) {
     int num = 1;
     System.out.println(Integer.toBinaryString(p20250221(num)));
+    System.out.println("Clear");
   }
 
   public static int p20250221(int n) {
